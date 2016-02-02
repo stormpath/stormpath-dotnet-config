52fe7a51aeed9a4aa49b8a4413ee6f1a34f853df
Nate Barbettini <nathanaelb@gmail.com>
2016-02-02 14:49:24 -0800

    Import code from stormpath-aspnet

---
+++ Stormpath.Configuration.Test/ConfigurationTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Stormpath.Configuration.Test
+{
+    public class ConfigurationTest
+    {
+        public static readonly StormpathConfiguration DefaultConfiguration = new StormpathConfiguration()
+        {
+            Application = new ApplicationConfiguration()
+            {
+                Name = "Foobar",
+                Href = null
+            }
+        };
+
+        [Fact]
+        public void MyTestMethod()
+        {
+            // Defaults
+            //var poco = new StormpathConfiguration()
+            //{
+            //    Application = new ApplicationConfiguration()
+            //    {
+            //        Name = "Lightsabers Galore!"
+            //    }
+            //};
+
+            var poco = new StormpathConfiguration();
+
+            var config = new ConfigurationBuilder()
+                //.SetBasePath("..\\")
+                .AddJsonFile("defaultConfig.json")
+                .Build();
+
+            poco.Application.Name = config.Get("application:name", DefaultConfiguration.Application.Name);
+            poco.Client.CacheManager.DefaultTtl = config.Get<int?>("client:cacheManager:defaultTtl");
+            poco.Client.CacheManager.Caches = config.Get<Dictionary<string, ClientCacheConfiguration>>("client:cacheManager:caches");
+
+            poco.Application.Name.Should().Be("Lightsabers Galore!");
+        }
+    }
+}
