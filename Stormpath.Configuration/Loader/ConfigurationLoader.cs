847e9b817d684dc04a63cebdcabfe716244c3f56
Nate Barbettini <nathanaelb@gmail.com>
2016-02-03 09:33:34 -0800

    OS detection and smart home paths

--- Stormpath.Configuration/Loader/ConfigurationLoader.cs
+++ Stormpath.Configuration/Loader/ConfigurationLoader.cs
@@ -14,15 +14,20 @@
 // limitations under the License.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Contrib.Stormpath.PropertiesFile;
+using Microsoft.Extensions.PlatformAbstractions;
 using Stormpath.Configuration.Model;
 
 namespace Stormpath.Configuration.Loader
 {
     internal sealed class ConfigurationLoader
     {
+        private static readonly string stormpathDirectory = $"~{Path.DirectorySeparatorChar}.stormpath{Path.DirectorySeparatorChar}";
+
         private readonly object userConfiguration;
 
         public ConfigurationLoader(object userConfiguration)
@@ -39,6 +44,8 @@ namespace Stormpath.Configuration.Loader
             BindApplicationSection(compiled, output);
             BindWebSection(compiled, output);
 
+            // If client.apiKey.file is set, load that. Throw if it doesn't exist
+
             // Validate API Key and Secret
 
             // Web Validation?
@@ -49,9 +56,9 @@ namespace Stormpath.Configuration.Loader
         private IConfigurationRoot CompileFromSources()
         {
             var builder = new ConfigurationBuilder()
-                .AddPropertiesFile("~/.stormpath/apiKey.properties", optional: true, prefix: "client")
-                .AddJsonFile("~/.stormpath/stormpath.json", optional: true)
-                //.AddYamlFile("~/.stormpath/stormpath.yaml", optional: true)
+                .AddPropertiesFile(ResolveHomePath($"{stormpathDirectory}apiKey.properties"), optional: true, prefix: "client")
+                .AddJsonFile(ResolveHomePath($"{stormpathDirectory}stormpath.json"), optional: true)
+                //.AddYamlFile($"{dotStormpathPath}stormpath.yaml", optional: true)
                 .AddPropertiesFile("apiKey.properties", optional: true, prefix: "client")
                 .AddJsonFile("stormpath.json", optional: true)
                 //.AddYamlFile("stormpath.yaml", optional: true)
@@ -70,7 +77,7 @@ namespace Stormpath.Configuration.Loader
 
             client.CacheManager.DefaultTtl = compiled.Get("client:cacheManager:defaultTtl", Default.Configuration.Client.CacheManager.DefaultTtl);
             client.CacheManager.DefaultTti = compiled.Get("client:cacheManager:defaultTti", Default.Configuration.Client.CacheManager.DefaultTti);
-            client.CacheManager.Caches = compiled.Get<Dictionary<string, ClientCacheConfiguration>>("client:cacheManager:caches");
+            client.CacheManager.Caches = compiled.Get("client:cacheManager:caches", new Dictionary<string, ClientCacheConfiguration>(Default.Configuration.Client.CacheManager.Caches));
 
             client.BaseUrl = compiled.Get("client:baseUrl", Default.Configuration.Client.BaseUrl);
             client.ConnectionTimeout = compiled.Get("client:connectionTimeout", Default.Configuration.Client.ConnectionTimeout);
@@ -91,6 +98,49 @@ namespace Stormpath.Configuration.Loader
         private void BindWebSection(IConfigurationRoot compiled, StormpathConfiguration output)
         {
             //todo
+        }
+
+        private string ResolveHomePath(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            if (!input.StartsWith("~"))
+            {
+                return input;
+            }
+
+            var homePath = GetHome();
+
+            return System.IO.Path.Combine(homePath, input.Replace($"~{System.IO.Path.DirectorySeparatorChar}", string.Empty));
+        }
+
+        // Copied from DNX's DnuEnvironment.cs
+        private static string GetHome()
+        {
+#if DNX451
+            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+#else
+            var runtimeEnv = PlatformServices.Default.Runtime;
+            if (runtimeEnv.OperatingSystem == "Windows")
+            {
+                return Environment.GetEnvironmentVariable("USERPROFILE") ??
+                    Environment.GetEnvironmentVariable("HOMEDRIVE") + Environment.GetEnvironmentVariable("HOMEPATH");
+            }
+            else
+            {
+                var home = Environment.GetEnvironmentVariable("HOME");
+
+                if (string.IsNullOrEmpty(home))
+                {
+                    throw new Exception("Home directory not found. The HOME environment variable is not set.");
+                }
+
+                return home;
+            }
+#endif
         }
     }
 }
