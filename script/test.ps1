81b3e6b83f89225516beee342b113888b52e961b
Nate Barbettini <nathanaelb@gmail.com>
2016-02-19 15:13:58 -0800

    Test script should fail if tests fail

--- script/test.ps1
+++ script/test.ps1
@@ -1,5 +1,18 @@
+$exitCode = 0
+
 dnx -p test\Microsoft.Extensions.Configuration.Contrib.Stormpath.EnvironmentVariables.Test test
+$exitCode = $LASTEXITCODE
+
 dnx -p test\Microsoft.Extensions.Configuration.Contrib.Stormpath.ObjectReflection.Test test
+$exitCode = $LASTEXITCODE
+
 dnx -p test\Microsoft.Extensions.Configuration.Contrib.Stormpath.PropertiesFile.Test test
+$exitCode = $LASTEXITCODE
+
 dnx -p test\Microsoft.Extensions.Configuration.Contrib.Stormpath.Yaml.Test test
+$exitCode = $LASTEXITCODE
+
 dnx -p test\Stormpath.Configuration.Test test
+$exitCode = $LASTEXITCODE
+
+return $exitCode
